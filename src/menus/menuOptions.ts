84cfab04c013bebc19790fead92dc643d0d85870
lmkindermann <lmkindermann@gmail.com>
2026-07-17 16:49:30 -0300

    feat: implementado o texto de todos os menus

---
+++ src/menus/menuOptions.ts
@@ -0,0 +1,56 @@
+export function menuPrincipal() {
+    console.clear();
+    console.log("Bem-vindo(a) ao Bookstore Manager");
+    console.log("Selecione a opção desejada:");
+    console.log("1. Autores");
+    console.log("2. Livros");
+    console.log("3. Clientes");
+    console.log("4. Empréstimos");
+    console.log("5. Encerrar")
+}      
+        
+export function menuAutor() {
+    console.clear();
+    console.log("Autores");
+    console.log("Selecione a opção desejada:");
+    console.log("1. Cadastrar autor");
+    console.log("2. Listar autores");
+    console.log("3. Consultar autor")
+    console.log("4. Atualizar autor");
+    console.log("5. Remover autor");
+    console.log("6. Retornar ao menu anterior");
+}
+
+export function menuLivro() {
+    console.clear();
+    console.log("Livros");
+    console.log("Selecione a opção desejada:");
+    console.log("1. Cadastrar livro");
+    console.log("2. Listar livros");
+    console.log("3. Consultar livro")
+    console.log("4. Atualizar livro");
+    console.log("5. Remover livro");
+    console.log("6. Retornar ao menu anterior");
+}
+
+export function menuCliente() {
+    console.clear();
+    console.log("Clientes");
+    console.log("Selecione a opção desejada:");
+    console.log("1. Cadastrar cliente");
+    console.log("2. Listar clientes");
+    console.log("3. Consultar cliente")
+    console.log("4. Atualizar cliente");
+    console.log("5. Remover cliente");
+    console.log("6. Retornar ao menu anterior");
+}
+
+export function menuEmprestimo() {
+    console.clear();
+    console.log("Empréstimos");
+    console.log("Selecione a opção desejada:");
+    console.log("1. Cadastrar empréstimo");
+    console.log("2. Consultar empréstimo")
+    console.log("3. Registrar devolução");
+    console.log("4. Retornar ao menu anterior");
+}
