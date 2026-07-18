6493c34b4d749ba7902f60dc00029c87bd7f1527
lmkindermann <lmkindermann@gmail.com>
2026-07-18 12:49:58 -0300

    feat: adicionado erro de conexão quando banco de dados ainda não foi criado

--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -21,14 +21,17 @@ export const connectToDatabase = async () => {
         const client = await pool.connect();
         console.log('Conexão com o banco de dados estabelecida com sucesso!');
         client.release(); // Libera o cliente de volta para o pool
-    } catch (error) {
-        console.error('Erro ao conectar ao banco de dados:', error);
-        //if (error.code === '3D000') {
-        //    console.log('AVISO! Banco de dados inexistente. Utilize o comando 5.1. para criá-lo.');
-        //    console.log('Na sequencia, reinicie a aplicação para verificar a conexão com o banco de dados.');
-        //} else {
-        //    console.error('Erro ao conectar ao banco de dados:', error);
-        //}
+        return '0';
+    } catch (error: any) {
+        //console.error('Erro ao conectar ao banco de dados:', error);
+        if (error.code === '3D000') {
+           console.log('AVISO! Banco de dados inexistente.');
+           console.log('\nSe este for o seu primeiro uso, crie o banco e as tabelas primeiro através das opções 5.1 e 5.2,');
+           console.log('Na sequencia, reinicie a aplicação para estabelecer a conexão com o banco de dados da aplicação.');         
+        } else {
+           console.error('Erro ao conectar ao banco de dados:', error);
+        }
+        return error.code;
     }
 }
 
